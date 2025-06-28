6b565e5cae93b5e0e7fa8c94eea731f1d1cc9b51
VinayAnupNair <vinayanupnair007@gmail.com>
2025-06-28 17:05:42 -0500

    experimented with tuples

--- src/main.rs
+++ src/main.rs
@@ -80,4 +80,19 @@ fn main() {
     // -- arrays
     
     // we start with tuples_
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // one way to unpack tuple
+    let (x,y,z) = tup;
+    
+    // printing out the values of the tuple we unpacked
+    println!("the value of x is: {x}");
+    println!("the value of y is: {y}");
+    println!("the value of z is: {z}");
+
+    // alt way of unpacking the tuple
+    let _five_hundred = tup.0;
+    let _six_point_four = tup.1;
+    let _one = tup.2;
+
 }
