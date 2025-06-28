e95be1fdea3be79a48e79a7611194e94924bc874
VinayAnupNair <vinayanupnair007@gmail.com>
2025-06-27 22:59:57 -0500

    added a constant

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,10 @@ fn main() {
 
     // printing out to verify the change
     println!("The value of x is: {x}");
+
+    // adding a constant
+    const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
+
+    // printing out the constant
+    println!("The value of the constant is: {THREE_HOURS_IN_SECONDS}");
 }
