ca234dd595c6f34770f3b88f0479ac49dcc6be1d
VinayAnupNair <vinayanupnair007@gmail.com>
2025-06-27 22:54:33 -0500

    addedmutable variable x

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+
+    // initializing the mutable variable x and assigning it the value 5
+    let mut x = 5;
+
+    // printing out to verify the value
+    println!("The value of x is: {x}");
+
+    // changing the value of the variable to 6
+    x = 6;
+
+    // printing out to verify the change
+    println!("The value of x is: {x}");
 }
